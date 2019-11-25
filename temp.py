75f9054ed795e23d15967d4bcbac34e16154a673
Felix Reidl <felix.reidl@gmail.com>
2019-11-25 11:35:26 +0000

    temp.py changed for new table

--- temp.py
+++ temp.py
@@ -2,26 +2,47 @@
 
 import sys, os
 
-total = 0
-leaves = 0
-dist = None
+def stats(file):
+    total = 0
+    leaves = 0
+    edges = 0
+    dist = None
 
-with open(sys.argv[1], 'r') as f:
-    for _ in range(3):
-        next(f)
-    l = next(f)
-    dist = int(l.split()[1])
-    for l in f:
-        l = l.rstrip()
+    with open(file, 'r') as f:
+        for _ in range(3):
+            next(f)
+        l = next(f)
+        dist = int(l.split()[1])
+        for l in f:
+            l = l.rstrip()
 
-        if l == '* Edges':
-            break
-        if l[0] == '*':
-            continue
-        total += 1
-        if '{' not in l:
-            leaves += 1
-name = os.path.basename(sys.argv[1])
-name = name.split('.')[0]
-print("{} & {} & {} & {} \\\\".format(name, total, leaves, dist))
+            if l == '* Edges':
+                break
+            if l[0] == '*':
+                continue
+            total += 1
+            if '{' not in l:
+                leaves += 1
+        for l in f:
+            l = l.rstrip()
+            if len(l) == 0:
+                continue
+            edges += len(l.split()) - 1
+    return total, leaves, edges, dist
 
+graphs = ['P{}'.format(i) for i in range(3,6)]
+graphs += ['C{}'.format(i) for i in range(4,7)]
+graphs += ['S{}'.format(i) for i in range(3,6)]
+graphs += ['W{}'.format(i) for i in range(3,7)]
+graphs += ['K{},{}'.format(i,i) for i in range(3,6)]
+
+# for H in graphs:
+#     name = H[0] + "_{" + H[1:] + "}"
+#     total, leaves, edges, dist = stats('example-graphs/{}.dag'.format(H))
+#     print("${}$ & {} ({}) & {} & {} \\\\".format(name, total, leaves, edges, dist))    
+
+n = 4
+for H in ['diamond', 'paw']:
+    total, leaves, edges, dist = stats('example-graphs/{}.dag'.format(H))
+    print("{} & {} & {} ({}) & {} & {} \\\\".format(n, H, total, leaves, edges, dist)) 
+    n = ''
